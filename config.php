32fcc12fe2777913a941af59d776060414eeac7f
pc <pc@aba-firas.local>
2020-02-05 23:04:05 +0100

    config modify

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /* Connexion a la base de donnée - On prepare les credentials*/
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'dogpal');
+define('DB_SERVER', 'ec2-54-246-89-234.eu-west-1.compute.amazonaws.com');
+define('DB_USERNAME', 'pdajnvgzagmvar');
+define('DB_PASSWORD', 'a5897ad12ebcb1f84b801acc618b7cd77bfc635cbc524878a10aa5e920b28fd8');
+define('DB_NAME', 'd7m1q2t4dp3612');
 
 /* Connection a la BDD MySQL */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
